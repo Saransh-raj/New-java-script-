71cdec9463fc169015aa5e295979a7c58b3d2a3e
Saransh Raj <saranshraj011@gmail.com>
2026-03-20 18:35:01 +0000

    comments add and table print

--- basics/variable.js
+++ basics/variable.js
@@ -1,5 +1,12 @@
 const email  = "saransh@gmail.com"
 let password = "12345"
 var number = "23456"
+let course;
 
-console.table([email,password,number])
+// email = "abc@gmail" // not allowed 
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([email,password,number, course])
