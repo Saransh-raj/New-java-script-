e9d82f0977bb541e264a039ab632378a341b481c
Saransh Raj <saranshraj011@gmail.com>
2026-03-20 18:58:22 +0000

    datatypes in java

---
+++ basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version
+
+// data types in JS
+
+/*  ~ premitive data type
+        number => 2 to the power 53
+        bigint
+        string => ""
+        boolean => true/false
+        null
+        undefined
+        symbol => unique
+        
+    ~ non-premitive data type
+        object
+        array
+        function
+*/
+
+console.log(typeof undefined) // it will print object type in java
+console.log(typeof null) // it will print object type in java
