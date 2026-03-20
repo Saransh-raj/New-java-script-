a58486311ee29f8efd7e0490d6a7f6a643a0a0b0
Saransh Raj <saranshraj011@gmail.com>
2026-03-20 19:20:26 +0000

    conversion of data types

---
+++ basics/conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 33
+
+console.log(typeof (score)) // number
+
+// conversion
+let valueInString = String(score) // converted to String 
+// (make sure write data type in capital in which you converting like in above capital S in string)
+console.log(typeof valueInString)
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 || false -> 0
+
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
