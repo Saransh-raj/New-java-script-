bb10049ae9ced983483f7a0854ccaec3c54c8be9
Saransh Raj <saranshraj011@gmail.com>
2026-03-23 20:13:07 +0000

    for of loop in java script

---
+++ basics/for_of_loop.js
@@ -0,0 +1,12 @@
+// for-of loop in java script
+const arr = [1,2,3,4,5]
+for (const element of arr) {
+    console.log(element)
+}
+console.log('--------------------------------------------')
+
+
+const greet = "Greet from saransh"
+for (const gre of greet) {
+    console.log(`greet from sarnsh ${gre}`)
+}
