6248d8df692ccdd7ab5abb4ffe046145f886c3aa
Saransh Raj <saranshraj011@gmail.com>
2026-03-23 19:57:45 +0000

    do while loop in java script

---
+++ basics/do_while.js
@@ -0,0 +1,13 @@
+let score = 1
+do{
+    console.log(`my score is ${score}`)
+    score++
+}while(score <= 10)
+
+console.log('---------------------------------------')
+// do while code runs atleast once even the condition is wrong after once run it will check the condition
+let marks = 10
+do{
+    console.log(`marks is ${marks}`)
+    marks++;
+}while(marks > 11)
