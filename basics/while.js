41a8b07a653be2201e4c0ee7426b15ea972088ae
Saransh Raj <saranshraj011@gmail.com>
2026-03-23 19:42:17 +0000

    while loop in java script

---
+++ basics/while.js
@@ -0,0 +1,13 @@
+let index = 0;
+while (index <= 10) {
+   console.log(`value of index is ${index}`) 
+   index +=2;
+}
+console.log("------------------------------------------------")
+// while for array
+ const myArr = ['batman','ironman','antman','spiderman']
+ let arr = 0
+ while(arr < myArr.length){
+    console.log(`value is ${myArr[arr]}`);
+    arr++;
+ }
